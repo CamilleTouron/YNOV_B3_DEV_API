8179ce9d3dcb0a34d28bdcf5a24ad2f1d8d891c4
Camille TOURON <camille.touron.pro@gmail.com>
2022-10-18 22:25:20 +0200

    ADD et DELETE de reviews.

--- Seance 4/projet-librairie/services/mocks/reviews.js
+++ Seance 4/projet-librairie/services/mocks/reviews.js
@@ -1,4 +1,7 @@
+const users = require("./users");
+const books = require("./books");
 const review1 = {
+    id:1,
     userId: 1,
     bookId: 3,
     note: "Nickel.",
@@ -6,6 +9,7 @@ const review1 = {
 };
 
 const review2 = {
+    id:1,
     userId: 2,
     bookId: 2,
     note: "Super.",
@@ -13,12 +17,100 @@ const review2 = {
 };
 
 const review3 = {
+    id:1,
     userId: 3,
     bookId: 1,
     note: "Bof.",
     date: new Date()
 };
 
-let reviews = [review1,review2,review3];
+let reviews = [review1, review2, review3];
 
-exports.reviews = reviews;
+
+//ADD
+const addReview = function (userId, bookId, note) {
+    if (isUserIdOk(userId)) {
+        return {
+            error: "userId does not exist"
+        }
+    }
+    if (isBookIdOk(bookId)) {
+        return {
+            error: "bookId does not exist"
+        }
+    }
+    if (!note) {
+        return {
+            error: "note is null"
+        }
+    }
+    const newReview = {
+        userId: userId,
+        bookId: bookId,
+        note: note,
+        date: new Date()
+    };
+    reviews.push(newReview);
+    return {
+        success: true,
+        addedReview: newReview
+    }
+
+};
+function isUserIdOk(userId) {
+    let result = true;
+    users.users.forEach(user => {
+        if (userId == user.id) {
+            result = false;
+            return;
+        }
+    });
+    console.log(result);
+    return result;
+}
+function isBookIdOk(bookId) {
+    let result = true;
+    books.books.forEach(book => {
+        if (bookId == book.id) {
+            result = false;
+            return;
+        }
+    });
+    console.log(result);
+    return result;
+}
+//DELETE
+const deleteReview = function (id) {
+        if (id && id < 0) {
+            return {
+                error: "id is not positive or id is null."
+            }
+        }
+        let result = null;
+        let i = 0;
+        reviews.forEach(review => {
+            if (id == review.id) {
+                result = review;
+                console.log()
+                reviews.splice(i, 1);
+                return;
+            }
+            i++;
+        });
+        if (result != null) {
+            return {
+                success: true,
+                deletedReview: result
+            }
+        } else {
+            return {
+                error: "The review you are trying to delete does not exist."
+            }
+        }
+
+
+    };
+
+    exports.reviews = reviews;
+    exports.addReview = addReview;
+    exports.deleteReview = deleteReview;
--- Seance 4/projet-librairie/services/routers/reviews.router.js
+++ Seance 4/projet-librairie/services/routers/reviews.router.js
@@ -2,8 +2,17 @@ const express = require('express');
 const reviewsRouter = express.Router();
 const reviews = require('../mocks/reviews');
 
+//GET
 reviewsRouter.get('/reviews',(req,res)=>{
     res.json(reviews);
+});
+//ADD
+reviewsRouter.post('/reviews',(req,res)=>{
+    res.json(reviews.addReview(req.body.userId,req.body.bookId,req.body.note));
+});
+//DALETE
+reviewsRouter.delete('/reviews/:id',(req,res)=>{
+    res.json(reviews.deleteReview(req.params.id));
 });
 
 module.exports=reviewsRouter;
