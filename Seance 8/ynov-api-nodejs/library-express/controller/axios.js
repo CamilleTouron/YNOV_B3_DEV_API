d8a6658c7c99991f34ccb0313a4ff6d12e697ee1
Camille TOURON <camille.touron.pro@gmail.com>
2022-11-14 11:17:31 +0100

    Client caching and some deletions.

--- Seance 8/ynov-api-nodejs/library-express/controller/axios.js
+++ Seance 8/ynov-api-nodejs/library-express/controller/axios.js
@@ -1,13 +1,14 @@
 const axios = require('axios');
 
+
 exports.getMeteo = (req, res, next) => {
    
     axios.get('https://api.open-meteo.com/v1/forecast?latitude=43.61&longitude=1.43&hourly=temperature_2m')
         .then(function (response) {
-            console.log('hola');
             console.log(response);
+            setClientCache(req, res, next);
+            console.log("end of get meteo");
             res.json({ success: true, res: response.data });
-            
         })
         .catch(function (error) {
             // handle error
@@ -16,4 +17,10 @@ exports.getMeteo = (req, res, next) => {
         .then(function () {
             // always executed
         });
+}
+
+function setClientCache(req, res, next) {
+    console.log("set client cache");
+    res.setHeader('TEST', 'CAMILLE');
+    next();
 }
--- Seance 8/ynov-api-nodejs/ressources.md
+++
@@ -1,9 +0,0 @@
-# Liste des ressources
-
-### Javascript
-- Linkedin learning (gratuit avec votre compte Ynov)
-- https://www.learn-js.org/
-
-### Node.JS
-- https://nodejs.dev/en/learn/
-- https://www.guru99.com/node-js-tutorial.html
